7b753a19b8f33e764a5a9f7f35ba15745c13ecd0
msiekelova <m.siekelova@gmail.com>
2022-04-24 17:21:09 +0200

    gitingore

--- PATO/src/main/java/cz/upol/pato/keycloak/config/KeycloakAdminClientUtils.java
+++ PATO/src/main/java/cz/upol/pato/keycloak/config/KeycloakAdminClientUtils.java
@@ -5,7 +5,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
